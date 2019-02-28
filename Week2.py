87af84ed25a95e9140f1d5859b89b28494f2abd8
mksreed <39208695+mksreed@users.noreply.github.com>
2019-02-27 23:18:59 -0500

    Add files via upload

--- Week2.py
+++ Week2.py
@@ -68,8 +68,8 @@ df.head()
 df=pd.read_csv('C:/Users/Owner/Documents/Python/olympics.csv')
 len(df)
 df=pd.read_csv('C:/Users/Owner/Documents/Python/olympics.csv', index_col = 0, skiprows=1)
-df.head()
-df.columns
+#df.head()
+#df.columns
 for col in df.columns: 
     if col[:2]=='01':
         df.rename(columns={col:'Gold' + col[4:]}, inplace=True)
@@ -79,23 +79,27 @@ for col in df.columns:
         df.rename(columns={col:'Bronze' + col[4:]}, inplace=True)
     if col[:1]=='№':
         df.rename(columns={col:'#' + col[1:]}, inplace=True) 
-df.head()
-df['Gold']>0
+df.drop('Totals')
 names_ids = df.index.str.split('\s\(') # split the index by '('
 
 df.index = names_ids.str[0] # the [0] element is the country name (new index) 
 df['ID'] = names_ids.str[1].str[:3] # the [1] element is the abbreviation or ID (take first 3 characters from that)
-
-df = df.drop('Totals')
 df.head()
 def answer_zero():
     return df.iloc[0]
 answer_zero() 
-df['# Summer'].idxmax()
-df['delta']=(df['# Summer']-df['# Winter'])
+df['Gold'].idxmax()
+df['delta']=abs(df['Gold']-df['Gold.1'])
+df['TotalGolds']=abs(df['Gold']+df['Gold.1'])
+df['relativedelta']=((df['Gold']-df['Gold.1']))/(df['TotalGolds'])
 df['delta'].idxmax()
-df['relativedelta']=((df['# Summer']-df['# Winter']))/((df['# Summer']+df['# Winter']))
-df['relativedelta'].idxmax()
+df1=(df[(df['Gold']>0) & (df['Gold.1']>0)])
+len(df)-len(df1)
+df1.columns
+df1['relativedelta'].idxmax()
+df['Points']=df['Gold.2']*3+df['Silver.2']*2+df['Bronze.2']
+s=pd.Series(df['Points'])
+type(s)
 #######################################################
 df = pd.read_csv('C:/Users/Owner/Documents/Python/log.csv')
 df
@@ -108,6 +112,31 @@ df = df.set_index(['time', 'user'])
 df = df.fillna(method='ffill')
 df.head
 #######################################################
+census_df=pd.read_csv('C:/Users/Owner/Documents/Python/census.csv')
+census_df.head()
+len(census_df)
+df0=census_df[census_df['COUNTY']>0]
+len(df0)
+df1=df0['STNAME']
+ans=df1.mode().iloc[0]
+
+
+df0=census_df[census_df['COUNTY']>0]
+df9=df0.sort_values(['STNAME', 'CENSUS2010POP'],ascending=False)
+df9[['STNAME','CENSUS2010POP']]
+
+df9.columns
+df10=df9[["REGION",'STNAME','CTYNAME','POPESTIMATE2014', 'POPESTIMATE2015']]
+df11=df10[df10['CTYNAME'].str.startswith('Washington')]
+df11['CTYNAME']
+
+
+census_df['SUMLEV'].unique()
+census_df['STATE'].unique()
+census_df.mode()['STATE'][0]
+census_df['STNAME'].groupby(lambda x: x).value_counts()
+census_df1=census_df[census_df['SUMLEV]==50]
+
 
 
 
